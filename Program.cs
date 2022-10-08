0d7602a9bf0964c5899e0f243af6ccc95ed93441
abdulkerimcetinkaya <113975750+abdulkerimcetinkaya@users.noreply.github.com>
2022-10-08 12:38:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace metodlar
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int a = 2;
+            int b = 3;
+            System.Console.WriteLine(a+b);
+
+            int sonuc = topla(a,b);
+            System.Console.WriteLine(sonuc);
+
+            metodlar ornek = new metodlar();
+            ornek.ekranaYazdır(Convert.ToString(sonuc));
+
+        }
+
+        static int topla(int deger1 , int deger2)
+        {
+            return (deger1 + deger2);
+        }
+    }
+
+    class  metodlar
+    {
+        public void ekranaYazdır(String veri)
+        {
+            System.Console.WriteLine(veri);
+        }
+    }
+    
+        
+    
+}
